8e239a0564fa2eb4cb5df0c116a577de685d01ba
ikramiste <ikrame.saadi@gmail.com>
2024-03-11 11:17:50 +0200

    pipotronic functional

--- lib/faussaire/bizness.rb
+++ lib/faussaire/bizness.rb
@@ -28,20 +28,28 @@ module Faussaire
     def self.format(arr)
       vowels = "aeiouyhéèà"
       arr.each_with_index do |phrase, i|
+        # Identifier si le mot actuel se termine par '#' et le traiter.
         if phrase[-1] == '#'
-          phrase.chop!
-          next_phrase_starts_with_vowel = vowels.include?(arr[i + 1][0].downcase)
-          arr[i] += next_phrase_starts_with_vowel ? 'e ' : "'"
-        elsif phrase[-1] == '\'' || (phrase.include?('#') && phrase[-3..] == "de ")
-          arr[i] = phrase[0...-1]
-          next_phrase_starts_with_vowel = vowels.include?(arr[i + 1][0].downcase)
+          phrase.chop! # Supprimer le dernier caractère '#'
+          next_phrase_starts_with_vowel = i < arr.size - 1 && vowels.include?(arr[i + 1][0].downcase)
           arr[i] += next_phrase_starts_with_vowel ? 'e ' : "'"
         end
-
+    
+        # Traiter les cas où le mot se termine par '\'' ou contient 'de ' nécessitant une décision sur l'ajout de 'e ' ou de ''.
+        if phrase[-1] == '\'' || (phrase.include?('de') && phrase[-3..] == "de ")
+          # Aucun besoin de couper le mot ici; ajustez simplement pour le suivant.
+          next_phrase_starts_with_vowel = i < arr.size - 1 && vowels.include?(arr[i + 1][0].downcase)
+          arr[i] += next_phrase_starts_with_vowel ? 'e ' : ""
+        end
+    
+        # Ajouter un espace si nécessaire.
         arr[i] += ' ' unless arr[i].end_with?(' ', "'") || i == arr.size - 1
+        # Traitement spécial pour le sixième élément, si nécessaire.
         arr[i] = 'les ' + arr[i] if i == 5
       end
-      arr.join.squeeze(' ').strip
+      # Construire la phrase et ajouter un point à la fin.
+      sentence = arr.join.squeeze(' ').strip + '.'
+      sentence
     end
   end
 end

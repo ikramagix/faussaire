112332a920c27a51d17c9249343debe3447a3902
ikramiste <ikrame.saadi@gmail.com>
2024-03-11 11:37:31 +0200

    pipotronic specs passed

--- spec/bizness_spec.rb
+++ spec/bizness_spec.rb
@@ -31,5 +31,50 @@ RSpec.describe Faussaire::Bizness do
             expect(duplicates).to be_empty, "Found duplicates in brands : #{duplicates.join(', ')}"
           end
         end
+
+        context 'in pipotronics list' do
+            let(:pipotronics) { Faussaire::Bizness.load_dico.flatten }
+        
+            it 'does not contain duplicates' do
+              duplicates = pipotronics.select { |item| pipotronics.count(item) > 1 }.uniq
+              expect(duplicates).to be_empty, "Found duplicates in pipotronics: #{duplicates.join(', ')}"
+            end
+          end
+    end
+
+    describe '.pipotronic' do
+        # Tester si la méthode retourne une chaîne de caractères
+        it 'returns a string' do
+          expect(Faussaire::Bizness.pipotronic).to be_a(String)
+        end
+        
+        # Tester si la chaîne retournée se termine par un point
+        it 'ends with a period' do
+          expect(Faussaire::Bizness.pipotronic).to end_with('.')
+        end
+    
+        # Tester si la chaîne contient au moins un espace, suggérant la présence de plusieurs mots
+        it 'contains spaces indicating multiple words' do
+          expect(Faussaire::Bizness.pipotronic).to include(' ')
+        end
+    
+        # Tester si la chaîne ne contient pas de marqueurs temporaires non remplacés comme '#'
+        it 'does not contain placeholder markers like #' do
+          expect(Faussaire::Bizness.pipotronic).not_to include('#')
+        end
+        
+        # Tester si la chaîne est encodée en UTF-8 et ne contient pas d'encodage invalide
+        it 'is encoded in UTF-8 and contains valid encoding' do
+          sentence = Faussaire::Bizness.pipotronic
+          expect(sentence.encoding.name).to eq("UTF-8")
+          expect(sentence.valid_encoding?).to be true
+        end
+    
+        # Tester la variabilité de la réponse (optionnel, peut être difficile à réaliser de manière fiable)
+        it 'generates variable output' do
+          sentence1 = Faussaire::Bizness.pipotronic
+          sentence2 = Faussaire::Bizness.pipotronic
+          expect(sentence1).not_to eq(sentence2)
+        end
     end
 end

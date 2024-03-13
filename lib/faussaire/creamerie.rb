3999e4455372765fee68e316fa58aea82243643d
ikramiste <ikrame.saadi@gmail.com>
2024-03-13 17:47:55 +0200

    creamerie data communication established

--- lib/faussaire/creamerie.rb
+++ lib/faussaire/creamerie.rb
@@ -2,13 +2,75 @@ require 'yaml'
 
 module Faussaire
   class Creamerie
-    DATA_PATH = File.expand_path('./../../locale/fr.yml', __dir__)
+    DATA_PATH = File.expand_path('../../locale/fr.yml', __dir__)
 
     class << self
+      ##
+      # Produces a random milk type.
+      #
+      # @return [String]
+      #
+      # @example
+      #   Faussaire::Creamerie.milk_type #=> "de vache"
+      def milk_type
+        fetch('fr.faussaire.creamerie.milk_type')
+      end
 
-        def name
-            fetch('fr.faussaire.creamerie.butter').sample
-        end
+      ##
+      # Produces a random butter.
+      #
+      # @return [String]
+      #
+      # @example
+      #   Faussaire::Creamerie.butter #=> "Beurre Charentes-Poitou"
+      def butter
+        fetch('fr.faussaire.creamerie.butter')
+      end
+
+      ##
+      # Produces a random cream.
+      #
+      # @return [String]
+      #
+      # @example
+      #   Faussaire::Creamerie.cream #=> "Crème d'Isigny"
+      def cream
+        fetch('fr.faussaire.creamerie.cream')
+      end
+
+      ##
+      # Produces a random cheese.
+      #
+      # @return [Hash]
+      #
+      # @example
+      #   Faussaire::Creamerie.cheese #=> { type: ..., name: ..., region: ... }
+      def cheese
+        {
+          type: fetch('fr.faussaire.creamerie.cheese.type'),
+          name: fetch('fr.faussaire.creamerie.cheese.name'),
+          region: fetch('fr.faussaire.creamerie.cheese.region')
+        }
+      end
+
+      private
+
+      def fetch(key)
+        data = YAML.load_file(DATA_PATH)
+        data.dig(*key.split('.')).sample
+      end
+
+      def cheese_type
+        fetch('creamerie.cheese.type')
+      end
+
+      def cheese_name
+        fetch('creamerie.cheese.name')
+      end
+
+      def cheese_region
+        fetch('creamerie.cheese.region')
+      end
     end
-end
+  end
 end
--- spec/creamerie_spec.rb
+++ spec/creamerie_spec.rb
@@ -1,8 +1,38 @@
+# spec/creamerie_spec.rb
 require 'spec_helper'
 require 'faussaire/creamerie'
 
 RSpec.describe Faussaire::Creamerie do
-    it 'exists' do 
-        puts "Hello world!"
+  shared_examples 'a valid category' do |method, expected_class|
+    it "#{method} returns an string of #{expected_class}" do
+      expect(Faussaire::Creamerie.send(method)).to be_an(String)
+      expect(Faussaire::Creamerie.send(method).first).to be_a(expected_class) unless Faussaire::Creamerie.send(method).empty?
     end
+  end
+
+  describe 'milk_type' do
+    it_behaves_like 'a valid category', :milk_type, String
+  end
+
+  describe 'butter' do
+    it_behaves_like 'a valid category', :butter, String
+  end
+
+  describe 'cream' do
+    it_behaves_like 'a valid category', :cream, String
+  end
+
+  describe 'cheese' do
+    it 'returns a hash with keys :type, :name, :region' do
+      expect(Faussaire::Creamerie.cheese).to all(be_a(Hash))
+      expect(Faussaire::Creamerie.cheese.first.keys).to include(:type, :name, :region)
+    end
+
+    it 'cheese types, names, and regions are strings' do
+      cheese = Faussaire::Creamerie.cheese.first
+      expect(cheese[:type]).to be_a(String)
+      expect(cheese[:name]).to be_a(String)
+      expect(cheese[:region]).to be_a(String)
+    end
+  end
 end

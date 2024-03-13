108fe2a8700fdd58313d42c02a9d18aa9f6c9907
ikramiste <ikrame.saadi@gmail.com>
2024-03-13 17:50:13 +0200

    rspecs done for creamerie

--- spec/creamerie_spec.rb
+++ spec/creamerie_spec.rb
@@ -1,35 +1,34 @@
-# spec/creamerie_spec.rb
 require 'spec_helper'
 require 'faussaire/creamerie'
 
 RSpec.describe Faussaire::Creamerie do
-  shared_examples 'a valid category' do |method, expected_class|
-    it "#{method} returns an string of #{expected_class}" do
-      expect(Faussaire::Creamerie.send(method)).to be_an(String)
-      expect(Faussaire::Creamerie.send(method).first).to be_a(expected_class) unless Faussaire::Creamerie.send(method).empty?
+  describe '.milk_type' do
+    it 'returns a string' do
+      expect(Faussaire::Creamerie.milk_type).to be_a(String)
     end
   end
 
-  describe 'milk_type' do
-    it_behaves_like 'a valid category', :milk_type, String
+  describe '.butter' do
+    it 'returns a string' do
+      expect(Faussaire::Creamerie.butter).to be_a(String)
+    end
   end
 
-  describe 'butter' do
-    it_behaves_like 'a valid category', :butter, String
+  describe '.cream' do
+    it 'returns a string' do
+      expect(Faussaire::Creamerie.cream).to be_a(String)
+    end
   end
 
-  describe 'cream' do
-    it_behaves_like 'a valid category', :cream, String
-  end
-
-  describe 'cheese' do
-    it 'returns a hash with keys :type, :name, :region' do
-      expect(Faussaire::Creamerie.cheese).to all(be_a(Hash))
-      expect(Faussaire::Creamerie.cheese.first.keys).to include(:type, :name, :region)
+  describe '.cheese' do
+    it 'returns a hash with :type, :name, and :region keys' do
+      cheese = Faussaire::Creamerie.cheese
+      expect(cheese).to be_a(Hash)
+      expect(cheese.keys).to contain_exactly(:type, :name, :region)
     end
 
-    it 'cheese types, names, and regions are strings' do
-      cheese = Faussaire::Creamerie.cheese.first
+    it 'returns strings for cheese type, name, and region' do
+      cheese = Faussaire::Creamerie.cheese
       expect(cheese[:type]).to be_a(String)
       expect(cheese[:name]).to be_a(String)
       expect(cheese[:region]).to be_a(String)

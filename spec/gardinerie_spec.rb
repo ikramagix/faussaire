5f2b7902b50a0f0672a6f941b74f44dae7471c6f
ikramiste <ikrame.saadi@gmail.com>
2024-03-15 12:04:22 +0200

    basic rspec for gardinerie done

---
+++ spec/gardinerie_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+require 'faussaire/gardinerie'
+
+RSpec.describe Faussaire::Gardinerie do
+    describe '.vegetable' do
+      it 'returns a string' do
+        expect(Faussaire::Gardinerie.vegetable).to be_a(String)
+      end
+  
+      it 'matches the expected format' do
+        expect(Faussaire::Gardinerie.vegetable).to match(/\A.+ \(.+\)\z/)
+      end
+    end
+  
+    describe '.fruit' do
+      it 'returns a string' do
+        expect(Faussaire::Gardinerie.fruit).to be_a(String)
+      end
+  
+      it 'matches the expected format' do
+        expect(Faussaire::Gardinerie.fruit).to match(/\A.+ \(.+\)\z/)
+      end
+    end
+  
+    describe '.rival_plant' do
+      it 'returns a string' do
+        expect(Faussaire::Gardinerie.rival_plant).to be_a(String)
+      end
+    end
+  
+    describe '.very_naughty_plant' do
+      it 'returns a string' do
+        expect(Faussaire::Gardinerie.very_naughty_plant).to be_a(String)
+      end
+    end
+  end
+  

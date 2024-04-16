e1126395c548027dfbd89e610d4562e571048881
ikramiste <ikrame.saadi@gmail.com>
2024-04-16 23:34:04 +0300

    refacto wine

--- lib/faussaire/wine.rb
+++ lib/faussaire/wine.rb
@@ -2,7 +2,7 @@ require 'yaml'
 
 module Faussaire
   class Wine
-    DATA_PATH = File.expand_path('./../../locale/fr.yml', __dir__)
+    DATA_PATH = File.expand_path('../../locale/fr.yml', __dir__)
 
     class << self
       ##
@@ -14,7 +14,7 @@ module Faussaire
       #   Faussaire::Wine.name #=> "Chateau Margaux"
       #
       def name
-        fetch('fr.faussaire.wine.name').sample
+        fetch('fr.faussaire.wine.name')
       end
 
       ##
@@ -26,7 +26,7 @@ module Faussaire
       #   Faussaire::Wine.type #=> "Red Wine"
       #
       def type
-        fetch('fr.faussaire.wine.type').sample
+        fetch('fr.faussaire.wine.type')
       end
 
       ##
@@ -38,7 +38,7 @@ module Faussaire
       #   Faussaire::Wine.bottle_type #=> "Cabernet Sauvignon"
       #
       def bottle_type
-        fetch('fr.faussaire.wine.bottle_types').sample
+        fetch('fr.faussaire.wine.bottle_types')
       end
 
       ##
@@ -50,9 +50,10 @@ module Faussaire
       #   Faussaire::Wine.region #=> "Bordeaux"
       #
       def region
-        fetch('fr.faussaire.wine.region').sample
+        fetch('fr.faussaire.wine.region')
       end
 
+      ##
       # Produces a random wine price with 99 cents and a Euro (€) symbol.
       # Distribution:
       # - 50% chance for a price between 3.5 and 499 (Low range)
@@ -67,29 +68,40 @@ module Faussaire
       #
       def price
         random_number = rand
-        case random_number
-        when 0...0.5
-          euros = rand(3.5..499).floor
-        when 0.5...0.85
-          euros = rand(500..2999).floor
-        when 0.85...0.95
-          euros = rand(3000..301300).floor
-        else
-          euros = rand(301301..482000).floor
-        end
-
+        euros = case random_number
+                when 0...0.5 then rand(3.5..499).floor
+                when 0.5...0.85 then rand(500..2999).floor
+                when 0.85...0.95 then rand(3000..301300).floor
+                else rand(301301..482000).floor
+                end
         "#{euros}.99€"
       end
 
+      ##
+      # Produces a random wine feature called "Licocorico".
+      #
+      # @return [String]
+      #
+      # @example
+      #   Faussaire::Wine.licocorico #=> "Licocorico Special Blend"
+      #
       def licocorico
-        fetch('fr.faussaire.wine.licocorico').sample
+        fetch('fr.faussaire.wine.licocorico')
       end
 
       private
 
+      ##
+      # Fetches and samples data from the YAML file based on the provided key.
+      # If the data is an array, it samples a single item.
+      #
+      # @param key [String] The dot-separated key used to access the data.
+      # @return [Object, nil] The data fetched and optionally sampled.
+      #
       def fetch(key)
         data = YAML.load_file(DATA_PATH)
-        data.dig(*key.split('.'))
+        result = data.dig(*key.split('.'))
+        result.is_a?(Array) ? result.sample : result
       end
     end
   end

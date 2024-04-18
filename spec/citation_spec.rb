89e7c8bb6fcdc041a9296ad2d1e9d0f1586161a0
ikramiste <ikrame.saadi@gmail.com>
2024-04-18 14:15:53 +0300

    Remove duplicate proverbs from locale file - fixed spec for duplicate check in proverbs

--- spec/citation_spec.rb
+++ spec/citation_spec.rb
@@ -18,6 +18,28 @@ RSpec.describe Faussaire::Citation do
         end
       end
 
+      context 'in proverbs list' do
+        let(:proverbs) { Faussaire::Citation.send(:fetch, 'fr.faussaire.citation.proverbe') }
+    
+        it 'does not contain duplicates' do
+          counts = Hash.new(0)
+          proverbs.each { |proverb| counts[proverb] += 1 }
+          duplicates = counts.select { |k, v| v > 1 }.keys
+          expect(duplicates).to be_empty, "Found duplicates in proverbs: #{duplicates.join(', ')}"
+        end
+    
+        it 'does not contain multiple consecutive spaces' do
+          proverbs.each do |proverb|
+            expect(proverb).not_to match(/\s{2,}/), "Found multiple consecutive spaces in proverb: #{proverb}"
+          end
+        end
+    
+        it 'does not contain empty strings' do
+          empty_proverbs = proverbs.select { |proverb| proverb.empty? }
+          expect(empty_proverbs).to be_empty, "Found empty strings in proverbs"
+        end
+      end    
+
       it 'does not contain empty strings' do
         empty_quotes = philoquotes.select { |quote| quote.empty? }
         expect(empty_quotes).to be_empty, "Found empty strings in philosophy quotes"

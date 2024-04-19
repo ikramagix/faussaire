d6dbc91483e5b4555520d4765ada463e9c281eb3
ikramiste <ikrame.saadi@gmail.com>
2024-04-19 14:36:40 +0300

    added duplicate specs for neighborhood & exploration

--- spec/cosmos_spec.rb
+++ spec/cosmos_spec.rb
@@ -23,6 +23,42 @@ RSpec.describe Faussaire::Cosmos do
       end
     end
 
+    context 'in cosmos neighborhood list' do
+      let(:neighborhood) { cosmos_data['fr']['faussaire']['cosmos']['neighborhood'] }
+
+      it 'does not contain duplicates' do
+        expect(neighborhood.uniq.count).to eq(neighborhood.count), "Found duplicates in cosmos neighborhood: #{neighborhood.group_by(&:itself).select { |k,v| v.size > 1 }.keys.join(', ')}"
+      end
+
+      it 'does not contain multiple consecutive spaces' do
+        neighborhood.each do |neighborhood|
+          expect(neighborhood).not_to match(/\s{2,}/), "Found multiple consecutive spaces in neighborhood name: #{neighborhood}"
+        end
+      end
+
+      it 'does not contain empty strings' do
+        expect(neighborhood).not_to include(''), "Found empty strings in planet names"
+      end
+    end
+
+    context 'in comsmic exploration list' do
+      let(:exploration) { cosmos_data['fr']['faussaire']['cosmos']['exploration'] }
+
+      it 'does not contain duplicates' do
+        expect(exploration.uniq.count).to eq(exploration.count), "Found duplicates in cosmos exploration: #{exploration.group_by(&:itself).select { |k,v| v.size > 1 }.keys.join(', ')}"
+      end
+
+      it 'does not contain multiple consecutive spaces' do
+        exploration.each do |exploration|
+          expect(exploration).not_to match(/\s{2,}/), "Found multiple consecutive spaces in neighborhood name: #{exploration}"
+        end
+      end
+
+      it 'does not contain empty strings' do
+        expect(exploration).not_to include(''), "Found empty strings in planet names"
+      end
+    end
+
     context 'in cosmos event list' do
       let(:events) { cosmos_data['fr']['faussaire']['cosmos']['event'] }
 
